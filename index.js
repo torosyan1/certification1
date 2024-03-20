4ca31615be7b13d9d7bc0939ba5772607c1477bf
Vahag Torosyan <vahag.torosyan@nrgaming.com>
2024-03-21 02:04:13 +0400

    fix

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const Jimp = require('jimp');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Parse URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/jpg', async (req, res) => {
+  try {
+
+    const { type, name } = req.body;
+    let path = null;
+
+    if (type === 'standart') {
+      path = './IMG_6346.PNG';
+    } else if (type === 'premium') {
+      path = './IMG_6347.PNG';
+    } else {
+      path = './IMG_6348.PNG'
+    }
+
+    // Load the image
+    await Jimp.read(path, async (err, image) => {
+      if (err) throw err;
+
+      // Load the font
+      await Jimp.loadFont(Jimp.FONT_SANS_128_BLACK).then(async (font) => {
+        // Add text to the image
+        image.print(font, 900, 1510, name);
+
+        // Save the modified image
+        await image.write('output.jpg', (err) => {
+          if (err) throw err;
+          console.log('Image with text saved.');
+          const filePath = './output.jpg'
+          const fileStream = fs.createReadStream(filePath);
+      
+          fileStream.pipe(res);
+        });
+      }).catch(err => {
+        console.error(err);
+      });
+    });
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
